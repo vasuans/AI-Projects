de709e65b1b4fc01e4ca8209d41e8ddcad04a9d1
srinivasu.addanki@yahoo.com <srinivasu.addanki@yahoo.com>
2024-01-02 23:35:56 -0500

    second commit

---
+++ sentiment_analysis.py
@@ -0,0 +1,17 @@
+import nltk
+from nltk.sentiment import SentimentIntensityAnalyzer
+
+def analyze_sentiment(text):
+    sia = SentimentIntensityAnalyzer()
+    sentiment_score = sia.polarity_scores(text)['compound']
+
+    if sentiment_score >= 0.05:
+        return "Positive"
+    elif sentiment_score <= -0.05:
+        return "Negative"
+    else:
+        return "Neutral"
+
+text_to_analyze = "I loved Animal movie!"
+result = analyze_sentiment(text_to_analyze)
+print(f"Sentiment: {result}")
